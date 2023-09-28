afb3809a203d2867154b489ae180c5707fe4a73c
JRS296 <jonathansamuel296@gmail.com>
2023-09-28 19:35:51 +0530

    Commit 1.0 - Fixed React Environment Variables - 28.09.2023 - 19:35

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,8 @@
 //Axios Setup - for communication with backend, you can use Fetch as well
 import axios from "axios";
-import config from "./config.json";
 
 // Server Client
-const client = axios.create({baseURL: config.BASE_URL}); //Azure IP
+const client = axios.create({baseURL: process.env.REACT_APP_API_URL}); //Azure IP
 
 //Dev Client
 //const client = axios.create({baseURL: 'http://localhost:3000/api'});
